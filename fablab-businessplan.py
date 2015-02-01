637953efa1f71b123bb28c8404b79219a6bd6b3e
Massimo Menichinelli <info@openp2pdesign.org>
2015-02-01 23:24:09 +0100

    Add structure and first styles

--- fablab-businessplan.py
+++ fablab-businessplan.py
@@ -7,6 +7,8 @@
 
 import xlsxwriter
 
+# Create document -------------------------------------------------------------
+
 # Create the file
 workbook = xlsxwriter.Workbook('FabLab-BusinessPlan.xlsx')
 
@@ -16,8 +18,38 @@ activities = workbook.add_worksheet('Activities')
 membership = workbook.add_worksheet('Membership')
 total = workbook.add_worksheet('Total')
 
+
+# Create styles -------------------------------------------------------------
+
+# Add a bold style to highlight heading cells
+bold_style = workbook.add_format()
+bold_style.set_font_color('white')
+bold_style.set_bg_color('F56A2F')
+bold_style.set_bold()
+
+# Add a total style to highlight total cells
+total_style = workbook.add_format()
+total_style.set_font_color('red')
+total_style.set_bg_color('FAECC5')
+total_style.set_bold()
+
+# Add a style for money
+money_style = workbook.add_format({'num_format': u'€#,##0'})
+# Add green/red color for positive/negative numbers
+#money_style.set_num_format('[Green]General;[Red]-General;General')
+# Add a number format for cells with money 
+#money_style.set_num_format('0 "dollar and" .00 "cents"')
+
+
+# Add content -------------------------------------------------------------
+
 # Add content to the Expenses worksheet
-expenses.write('A1', 'Hello world')
+expenses.write('A1', 'Hello world', bold_style)
+expenses.write('A2', '12.33', money_style)
+expenses.write('A3', 'Total', total_style)
+
+
+# Save document -------------------------------------------------------------
 
 # Save and close the file
 workbook.close()
