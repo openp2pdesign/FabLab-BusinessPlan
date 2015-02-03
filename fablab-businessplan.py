4167c8973f443de77dc466df85c58540861f3f1f
Massimo Menichinelli <info@openp2pdesign.org>
2015-02-03 10:10:54 +0100

    Rearrange structure of worksheets with notes

--- fablab-businessplan.py
+++ fablab-businessplan.py
@@ -13,10 +13,16 @@ import xlsxwriter
 workbook = xlsxwriter.Workbook('FabLab-BusinessPlan.xlsx')
 
 # Create the worksheets
-expenses = workbook.add_worksheet('Expenses')
-activities = workbook.add_worksheet('Activities')
-membership = workbook.add_worksheet('Membership')
-total = workbook.add_worksheet('Total')
+executive = workbook.add_worksheet('Executive Summary')
+company = workbook.add_worksheet('Company Summary')
+market = workbook.add_worksheet('Market Analysis')
+
+# Add a worksheet for each year
+starting_year = 2015
+years_number = 5
+years = {}
+for i in range(years_number):
+    years[2015+i] = workbook.add_worksheet('Plan for '+str(2015+i))
 
 
 # Create styles -------------------------------------------------------------
@@ -50,14 +56,43 @@ total_style.set_bg_color('FAECC5')
 item_value = 0.00
 
 # Set the width of the columns
-expenses.set_column('A:A', 10)
+executive.set_column('A:A', 10)
 
 # Add content to the Expenses worksheet
-expenses.write('A1', 'Hello world', bold_style)
-expenses.write('A2', item_value, money_style)
-expenses.write('A3', item_value, money_style)
-expenses.write('A4', '=SUM(A2:A3)', money_total_style)
+executive.write('A1', 'Hello world', bold_style)
+executive.write('A2', item_value, money_style)
+executive.write('A3', item_value, money_style)
+executive.write('A4', '=SUM(A2:A3)', money_total_style)
 
+# Notes for the structure:
+
+# Executive summary
+# Introduction
+# Bar chart for years
+# Objectives
+# Mission
+# Keys to success
+
+# Company summary
+# Company structure/ownership
+# Start-up summary
+# Bar chart for start-up costs
+
+# Market analysis
+# Market segmentation
+# Pie chart for market segmentation
+# Target Market Segment Strategy
+
+# Strategy
+# Communication
+# Years forecast
+# Chart years forecast
+
+# Management
+# Persons
+# Years forecast
+
+# Financial plan
 
 # Save document -------------------------------------------------------------
 
